e57a9f4e75f91bef3355bc7143fb60ce48251452
Alex Beznos <beznos@sent.com>
2023-01-10 23:42:42 +0200

    Remove mount of health engine

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,17 @@
+class HealthController < ActionController::API
+  def show
+    if authenticated?
+      status = HealthMonitor.check(request: request, params: {providers: [params[:provider]]})
+      render json: status.to_json, status: status[:status]
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
+
+  private
+
+  def authenticated?
+    request.headers["Honeybadger-Token"] ==
+      Rails.application.credentials.honeybadger.header
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  with_honeybadger_auth =
-    lambda do |app|
-      Rack::Builder.new do
-        use HoneybadgerTokenAuth
-        run app
-      end
-    end
-
-  mount with_honeybadger_auth.call(HealthMonitor::Engine), at: "health"
   mount Sidekiq::Web => "admin/sidekiq"
 
   root "job_posts#index"
 
   get "search", to: "job_posts#search", as: :search
+  get "health/:provider/check", to: "health#show"
 end
