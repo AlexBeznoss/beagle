22c23c8224582ddba752f2d500019c783836412c
Alex Beznos <beznos@sent.com>
2023-02-14 22:19:33 +0200

    Comment rubyjobboard

--- app/sidekiq/enqueue_scrapers_job.rb
+++ app/sidekiq/enqueue_scrapers_job.rb
@@ -4,7 +4,7 @@ class EnqueueScrapersJob
   def perform
     ScrapeJob.perform_async("gorails")
     ScrapeJob.perform_async("remoteok")
-    ScrapeJob.perform_async("rubyjobboard")
+    # ScrapeJob.perform_async("rubyjobboard")
     (1..3).to_a.each do |page|
       ScrapeJob.perform_async("rubyonremote", page)
     end
