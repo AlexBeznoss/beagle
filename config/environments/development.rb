d66797b82b89778a81f9ba77ffc77ba65c7fce6b
Alex Beznos <beznos@sent.com>
2023-05-02 16:54:10 +0300

    Add hosts to dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # allow local dev on dot.test domain
+  config.hosts << /[a-z0-9]+\.test/
 end
