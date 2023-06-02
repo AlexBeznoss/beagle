339650e55f6fc3cabe8357a484bd10157b497da6
Alex Beznos <beznos@sent.com>
2023-06-02 17:24:53 +0300

    Add user tracking

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/utils", under: "utils"
 pin "tailwindcss-stimulus-components", to: "https://ga.jspm.io/npm:tailwindcss-stimulus-components@3.0.4/dist/tailwindcss-stimulus-components.modern.js"
+pin "highlight.run", to: "https://unpkg.com/highlight.run/dist/index.es.js"
