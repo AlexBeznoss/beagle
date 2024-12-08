0931d20b8d00b7f257a9b593b05130df69210baa
Alex Beznos <beznos@sent.com>
2024-12-08 17:37:53 +0200

    FIx

--- config/initializers/clerk.rb
+++ config/initializers/clerk.rb
@@ -2,5 +2,5 @@ Clerk.configure do |c|
   c.api_key = ENV["CLERK_SECRET_KEY"]
   c.middleware_cache_store = Rails.cache
   c.logger = Logger.new($stdout)
-  c.excluded_routes = ["/health/fly"]
+  c.excluded_routes = ["/health/*"]
 end
